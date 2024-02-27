94a2405c681a8f6f7161f1faa81ccc3c22ce9d66
cicerob98 <cicerobn1@gmail.com>
2024-02-27 18:18:58 -0300

    feat: add "requirements.txt", new function "total messages"

--- automessage.py
+++ automessage.py
@@ -6,33 +6,43 @@ from tkinter import *
 
 def typing():
     sleep(5)
-    global entry
-    string = entry.get()
-    while True:
-        pyautogui.write(string, interval= 0.1)
+    message = message_entry.get()
+    total_messages = int(text_entry.get())
+
+    while total_messages > 0:
+        pyautogui.write(message, interval= 0.1) # it will write the typed message with interval to simulate a user typing
         pyautogui.press('enter')
-        if keyboard.is_pressed('esc'):
-            break
+        total_messages -= 1
+        
+        try:
+                if keyboard.is_pressed('esc'): # it will stop working by clicking 'esc' you can change to to any key
+                    break
+                else:
+                    pass
+        finally:
+            pass
 
 interface = Tk()
 
-interface.title("Auto Message")
-text = Label(interface, text="Digite qual mensagen deseja enviar e clique no botão abaixo para iniciar.")
-interface.minsize(405, 120)
-interface.maxsize(405, 120)
-text.grid(column=0, row=0, padx=10, pady=10)
+interface.minsize(323, 220) # set min width and height
+interface.maxsize(323, 220) # set max width and height
 
-entry = Entry(interface, width= 50)
-entry.grid(column=0, row=1, padx=10, pady=10)
-entry.focus_set()
+interface.title("Auto Message")
+message_quest = Label(interface, text="Digite qual mensagem deseja enviar.")
+message_quest.grid(column=0, row=0, padx=10, pady=10)
+
+message_entry = Entry(interface, width = 50)
+message_entry.grid(column=0, row=1, padx=10, pady=10)
+message_entry.focus_set()
+
+
+text = Label(interface, text="Digite a quantidade de mensagens que deseja enviar.")
+text.grid(column=0, row=2, padx=10, pady=10)
+
+text_entry = Entry(interface, width = 20)
+text_entry.grid(column=0, row=3, padx=10, pady=10)
 
 bt = Button(interface, text="Escrever Mensagens", command=typing)
-bt.grid(column=0, row=2, padx=10, pady=10)
+bt.grid(column=0, row=4, padx=10, pady=10)
 
 interface.mainloop()
-
-
-# if __name__ == "__main__":
-#     typing()
-#-------------------------------------------------------#
-# pyautogui.click(601, 998) auto click na coordenada
